9fe9bbf170c0b8a383b3b14a703b736ad2e99fb3
Tida Mathida <66160066@go.buu.ac.th>
2026-03-10 00:36:48 +0700

    แก้ให้หน้าลบ แสดงรูป

--- flutter_application_1/lib/pages/deleteItem_page.dart
+++ flutter_application_1/lib/pages/deleteItem_page.dart
@@ -42,7 +42,60 @@ class _DeleteItemPageState extends State<DeleteItemPage> {
             margin: EdgeInsets.all(10),
 
             child: ListTile(
-              leading: Image.asset("assets/desk.jpg", width: 50),
+              leading: item.image != null && item.image != ""
+                  ? ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Image.network(
+                        "${ApiService.baseUrl}/uploads/${item.image}",
+                        width: 50,
+                        height: 50,
+                        fit: BoxFit.cover,
+                        headers: {
+                          "ngrok-skip-browser-warning": "true",
+                        },
+                        loadingBuilder: (context, child, loadingProgress) {
+                          if (loadingProgress == null) return child;
+                          return Container(
+                            width: 50,
+                            height: 50,
+                            color: Colors.grey[200],
+                            child: Center(
+                              child: CircularProgressIndicator(
+                                value: loadingProgress.expectedTotalBytes != null
+                                    ? loadingProgress.cumulativeBytesLoaded /
+                                        loadingProgress.expectedTotalBytes!
+                                    : null,
+                              ),
+                            ),
+                          );
+                        },
+                        errorBuilder: (context, error, stackTrace) {
+                          return Container(
+                            width: 50,
+                            height: 50,
+                            color: Colors.grey[200],
+                            child: Icon(
+                              Icons.broken_image,
+                              color: Colors.grey[500],
+                              size: 30,
+                            ),
+                          );
+                        },
+                      ),
+                    )
+                  : Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Icon(
+                        Icons.devices,
+                        size: 30,
+                        color: Colors.grey[600],
+                      ),
+                    ),
 
               title: Text(item.name),
 
